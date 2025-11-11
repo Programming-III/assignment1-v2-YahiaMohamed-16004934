af509ccb10ad93d08059baff0607354ac7d8b094
Yahia Mohamed_16004934 <yahiamohamed2341@gmail.com>
2025-11-11 09:48:35 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -2,9 +2,17 @@
 #define INSTRUCTOR_H
 
 #include "Person.h"
+#include <string>
 using namespace std;
-//#write Instructor class here
-
+class Instructor {
+private: 
+	string department;
+	int experienceYears;
+public:
+	Instructor();
+	Instructor(string department, int experienceYears);
+	void display();
+ };
 
 
 
